944edd6d5e9d175add5cf2d04c4539a70af97ea6
VietAnh14 <polaris140698@gmail.com>
2018-11-22 00:09:27 +0700

    Created ConnectDB class in DAL

--- BookStore/src/DAL/ConnectDB.java
+++ BookStore/src/DAL/ConnectDB.java
@@ -5,10 +5,53 @@
  */
 package DAL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Viet Anh
  */
 public class ConnectDB {
-    
+    private static String username = "sa";
+    private static String password = "datchim147";
+    private static String url = "jdbc:sqlserver://localhost:1433;databaseName=yourDBname;";
+    private static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+   public static Connection getConnection(){
+       try {
+           Class.forName(driver);
+           Connection cn = DriverManager.getConnection(url, username, password);
+           return cn;
+       } catch (ClassNotFoundException | SQLException e) {
+           e.printStackTrace();
+           return null;
+       }
+   }
+   
+   public static void closeCn(Connection cn, PreparedStatement ps, ResultSet rs){
+       try {
+           if (rs != null && !rs.isClosed()) {
+               rs.close();
+           }
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+       try {
+           if (ps != null && !ps.isClosed()) {
+               ps.close();
+           }
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+       try {
+           if (cn != null && !cn.isClosed()) {
+               cn.close();
+           }
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+   }
 }
